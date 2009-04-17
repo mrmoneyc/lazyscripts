983421de00fa35b47dc4c5179b88c151da47a82b
billy3321 <billy3321@gmail.com>
2009-04-17 16:46:19 +0800

    Accept Jesse Sung's patch to modified add_official_repos of openSUSE

--- distrib/openSUSE/add_official_repos.py
+++ distrib/openSUSE/add_official_repos.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 import os
+from lazyscripts import get_version
 
 def main():
-    pass
+    distro, codename = get_version()
+    comps = [ "oss", "non-oss" ]
+    for comps in comps:
+
+        os.system("zypper ar
+                http://ftp.twaren.net/Linux/OpenSuSE/distribution/" + codename
+                + "/repo/" + comps + "/ lazyscripts-" comps )
+        os.system("zypper mr -p 10 lazyscripts-" + comps )
+    os.system("zypper refresh")
 
 
 
