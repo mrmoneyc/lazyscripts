887f06ec7e73966b86022e8f0481a210561fa41c
billy3321 <billy3321@gmail.com>
2009-04-17 19:05:11 +0800

    Add zypper rr and replace uri as default uri

--- distrib/openSUSE/add_official_repos.py
+++ distrib/openSUSE/add_official_repos.py
@@ -3,11 +3,11 @@ import os
 from lazyscripts import info
 
 def main():
-    uri = "http://ftp.isu.edu.tw/pub/Linux/opensuse/distribution/"
+    uri = "http://download.opensuse.org/distribution/"
     distro, codename = info.get_distro()
     comps = [ "oss", "non-oss" ]
     for comps in comps:
-
+        os.system("zypper rr lazyscripts-" + comps )
         os.system("zypper ar " + uri + codename  + "/repo/" + comps + "/ lazyscripts-" + comps )
         os.system("zypper mr -p 10 lazyscripts-" + comps )
     os.system("zypper refresh")
