e4a2dc68d51c96fd36702482c42ad30d07e3a5d2
billy3321 <billy3321@gmail.com>
2009-04-17 21:19:25 +0800

    use yum to install gksu

--- distrib/Fedora/run_first.sh
+++ distrib/Fedora/run_first.sh
@@ -14,7 +14,7 @@ fi
 case $PLAT_NAME in
     x86_64|i386)
     wget http://lazyscripts.googlecode.com/files/gksu-lastest.${PLAT_NAME}.rpm
-    rpm -Uvh gksu-lastest.${PLAT_NAME}.rpm
+    yum -y localinstall gksu-lastest.${PLAT_NAME}.rpm
     ;;
     *)
     echo "Lazyscripts not support $PLAT_NAME"
