878e1f864c853982baf0e8de9e2b1d493bf6ca05
billy3321 <billy3321@gmail.com>
2009-04-17 16:59:51 +0800

    change repository uri

--- distrib/openSUSE/add_official_repos.py
+++ distrib/openSUSE/add_official_repos.py
@@ -3,11 +3,12 @@ import os
 from lazyscripts import info
 
 def main():
+    uri = "http://ftp.isu.edu.tw/pub/Linux/opensuse/distribution/"
     distro, codename = info.get_distro()
     comps = [ "oss", "non-oss" ]
     for comps in comps:
 
-        os.system("zypper ar http://ftp.twaren.net/Linux/OpenSuSE/distribution/" + codename  + "/repo/" + comps + "/ lazyscripts-" + comps )
+        os.system("zypper ar " + uri + codename  + "/repo/" + comps + "/ lazyscripts-" + comps )
         os.system("zypper mr -p 10 lazyscripts-" + comps )
     os.system("zypper refresh")
 
