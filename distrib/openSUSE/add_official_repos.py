a7988cab3e6469477a458fea5a8351a543c28c54
billy3321 <billy3321@gmail.com>
2009-04-17 16:53:45 +0800

    fix syntax error

--- distrib/openSUSE/add_official_repos.py
+++ distrib/openSUSE/add_official_repos.py
@@ -7,7 +7,7 @@ def main():
     comps = [ "oss", "non-oss" ]
     for comps in comps:
 
-        os.system("zypper ar http://ftp.twaren.net/Linux/OpenSuSE/distribution/" + codename  + "/repo/" + comps + "/ lazyscripts-" comps )
+        os.system("zypper ar http://ftp.twaren.net/Linux/OpenSuSE/distribution/" + codename  + "/repo/" + comps + "/ lazyscripts-" + comps )
         os.system("zypper mr -p 10 lazyscripts-" + comps )
     os.system("zypper refresh")
 
