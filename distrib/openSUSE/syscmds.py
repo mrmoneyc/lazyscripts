7c628836f026097803fb8033c08edac065e10de7
billy3321 <billy3321@gmail.com>
2009-04-17 14:43:46 +0800

    fix zypper arguments

--- distrib/openSUSE/syscmds.py
+++ distrib/openSUSE/syscmds.py
@@ -4,8 +4,8 @@ Here store the command which only in openSUSE
 """
 
 detect_pack = "rpm -q "
-install_cmd = "zypper -y install "
-remove_cmd = "zypper -y remove "
+install_cmd = "zypper -l --yes install "
+remove_cmd = "zypper --yes remove "
 network_config = ""
 repo_config = ""
 
