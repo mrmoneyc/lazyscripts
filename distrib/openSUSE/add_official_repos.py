a0aff5465745546f69f04be957b2503b4a9d1847
billy3321 <billy3321@gmail.com>
2009-04-17 19:07:08 +0800

    add zypper argument

--- distrib/openSUSE/add_official_repos.py
+++ distrib/openSUSE/add_official_repos.py
@@ -7,10 +7,10 @@ def main():
     distro, codename = info.get_distro()
     comps = [ "oss", "non-oss" ]
     for comps in comps:
-        os.system("zypper rr lazyscripts-" + comps )
-        os.system("zypper ar " + uri + codename  + "/repo/" + comps + "/ lazyscripts-" + comps )
-        os.system("zypper mr -p 10 lazyscripts-" + comps )
-    os.system("zypper refresh")
+        os.system("zypper -n rr lazyscripts-" + comps )
+        os.system("zypper -n ar " + uri + codename  + "/repo/" + comps + "/ lazyscripts-" + comps )
+        os.system("zypper -n mr -p 10 lazyscripts-" + comps )
+    os.system("zypper -n refresh")
 
 
 
