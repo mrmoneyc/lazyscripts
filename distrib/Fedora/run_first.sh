97acb2f99fe05967fb870668bdcfb8e15447112b
billy3321 <billy3321@gmail.com>
2009-04-17 22:08:24 +0800

    use rpm to install

--- distrib/Fedora/run_first.sh
+++ distrib/Fedora/run_first.sh
@@ -14,9 +14,9 @@ fi
 case $PLAT_NAME in
     x86_64|i386)
     wget http://lazyscripts.googlecode.com/files/libgksu-lastest.${PLAT_NAME}.rpm
-    yum -y --nogpgcheck localinstall libgksu-lastest.${PLAT_NAME}.rpm
     wget http://lazyscripts.googlecode.com/files/gksu-lastest.${PLAT_NAME}.rpm
-    yum -y --nogpgcheck localinstall gksu-lastest.${PLAT_NAME}.rpm
+#    yum -y --nogpgcheck localinstall libgksu-lastest.${PLAT_NAME}.rpm gksu-lastest.${PLAT_NAME}.rpm
+    rpm -Uvh libgksu-lastest.${PLAT_NAME}.rpm gksu-lastest.${PLAT_NAME}.rpm
     ;;
     *)
     echo "Lazyscripts not support $PLAT_NAME"
